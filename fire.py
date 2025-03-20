275e546313cddaba906bee4900c0697fce457763
Korim-itz-Brand <parsonalgit430@gmail.com>
2025-03-20 09:18:12 +0600

    Update fire.py

--- fire.py
+++ fire.py
@@ -4,5 +4,5 @@ try:
 except:
     os.system('pip install requests')
 os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
-import fire
-fire.__Main_()
+import file
+file.____Main___()
